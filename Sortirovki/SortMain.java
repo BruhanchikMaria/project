7043b8e6a426ba572718c7dbc01cfa792c9ad197
BruhanchikMaria <bruhanchik_maria@mail.ru>
2020-02-20 21:04:11 +0300

    Задание полностью

---
+++ Sortirovki/SortMain.java
@@ -0,0 +1,44 @@
+package Sortirovka;
+
+public class SortMain {
+    public static int[] insertionSort(int array[]) {
+        for (int left = 0; left < array.length; left++) {
+            int value = array[left];
+            int i = left - 1;
+            for (; i >= 0; i--) {
+                if (value < array[i]) {
+                    array[i + 1] = array[i];
+                } else {
+                    break;
+                }
+            }
+            array[i + 1] = value;
+        }
+        return array;
+    }
+    // 3.4.Сортировка слиянием
+    public static int[] mergeSort(int array[],int left,int right){
+
+        int delimiter = left + ((right - left) / 2) + 1;
+        // Выполним рекурсивно данную функцию для двух половинок (если сможем разбить(
+        if (delimiter > 0 && right > (left + 1)) {
+            mergeSort(array, left, delimiter - 1);
+            mergeSort(array, delimiter, right);
+        }
+        // Создаём временный массив с нужным размером
+        int[] buffer = new int[right - left + 1];
+        // Начиная от указанной левой границы идём по каждому элементу
+        int cursor = left;
+        for (int i = 0; i < buffer.length; i++) {
+            // Мы используем delimeter чтобы указывать на элемент из правой части
+            // Если delimeter > right, значит в правой части не осталось недобавленных элементов
+            if (delimiter > right || array[cursor] > array[delimiter]) {
+                buffer[i] = array[cursor];
+                cursor++;
+            } else {
+                buffer[i] = array[delimiter];
+                delimiter++;
+            }
+        }
+        return array;
+}}
